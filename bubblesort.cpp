4946d4f56e5031c83c22387e229b72f491a484db
abhir262 <abhi.ranarajput262@gmail.com>
2022-04-18 11:50:47 +0530

    demofile

---
+++ bubblesort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void bubblesort(int a[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+          if(a[j]>a[j+1]){
+              int temp=a[j];
+              a[j]=a[j+1];
+              a[j+1]=temp;
+                        }
+                                }
+    }
+}
+int main(){
+    int a[]={3,1,6,9,0,4};
+    bubblesort(a,6);
+    //cin>>a;
+    cout<<a<<endl;
+    for(int i=0;i<6;i++){
+        cout<<a[i]<<endl;
+    }
+}
---
+++ dynamic.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int *a=new int;
+    // *a=55;
+    // cout<<"random:"<<*a<<" "<<a<<endl;
+    // delete a;
+    // cout<< sizeof(a)<<"size: of &"<<sizeof(*a)<<endl;
+    // char *x= new char;
+    // *x='r';
+    // cout<<x<<" "<<"size of x:"<<sizeof(x)<<" "<<*x<<"size of*x:"<<sizeof(*x);
+//     int n;
+//     cin>>n;
+//     int* b=new int[n];
+//     cout<<"o1:"<<b<<"ad"<<endl;
+//     for(int i=0;i<n;i++){
+//         b[i]=i+2;
+//     }
+//     //cout<<"size of b:"<<*(b+1)<<endl;
+//     delete []b;
+//     int y=88;
+//     int* h=&y;
+//     b=h;
+// cout<<"B:"<<*h;
+ //cout<<"size of w:"<<sizeof(w);
+ int m;
+ int n;
+ cin>>m;
+ int**p=new int*[m];
+    for(int i=0;i<m;i++)  {
+      p[i]=new int[i+1];
+   for(int j=0;j<=i;j++){
+        cin>>p[i][j];
+                        }
+                             }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<=i;j++){
+            cout<<p[i][j]<<" ";
+                             }
+        cout<<endl;
+                        }                   
+for(int i=0;i<m;i++){
+    delete []p[i];
+                    }
+            delete []p; 
+            cout<<p<<endl<<p[0][0] ;      
+
+}
